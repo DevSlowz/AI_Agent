82e75942519fec2555983750708ef06f8d28ab93
DevSlowz <msvieira021@gmail.com>
2025-07-02 19:26:03 -0400

    Add feature to accept prompts from the command line

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 # Load enviorment variables
 load_dotenv()
@@ -11,12 +12,17 @@ client = genai.Client(api_key=api_key)
 
 def main():
     print("Hello from ai-agent!")
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-    )
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    # print(sys.argv)
+    if len(sys.argv) == 1:
+        print("ERROR: Prompt not provided")
+        exit(1)
+    else:
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-001', contents=sys.argv[1]
+        )
+        print(response.text)
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()

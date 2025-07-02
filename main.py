18b808f69e76a09a34903085eb990f8cf97d862e
DevSlowz <msvieira021@gmail.com>
2025-07-02 19:33:03 -0400

    Add message list which will hold the conversation between user and ai

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 # Load enviorment variables
@@ -11,14 +12,19 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
+
+
     print("Hello from ai-agent!")
     # print(sys.argv)
     if len(sys.argv) == 1:
         print("ERROR: Prompt not provided")
         exit(1)
     else:
+        messages = [
+            types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+        ]
         response = client.models.generate_content(
-            model='gemini-2.0-flash-001', contents=sys.argv[1]
+            model='gemini-2.0-flash-001', contents=messages,
         )
         print(response.text)
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")

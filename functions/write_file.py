a81d980bd0c2b91764507b397fe7dad1ce787649
DevSlowz <msvieira021@gmail.com>
2025-07-23 18:16:22 -0400

    Fix logic behind verify_path and added return statements for invalid and valide cases

--- functions/write_file.py
+++ functions/write_file.py
@@ -7,29 +7,33 @@ def write_file(working_directory, file_path, content):
 
 
 
-def verify_file_path_w(working_directory, file_path):
+def verify_file_path_w(working_directory, file_path, content):
+    
+
     abs_working_dir = os.path.abspath(working_directory)
-    # abs_file = os.path.abspath(file_path)
     abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    parent_dir = os.path.dirname(abs_file_path)
     common_path = [abs_working_dir, abs_file_path]
    
-    print(f'Abs file path = {abs_file_path}')
     in_scope = os.path.commonpath(common_path) == abs_working_dir
-    print(f"Common Path = {common_path}")
-    print(f'In Scope - {in_scope}')
+
     if not in_scope:
         return (False, f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory')
 
     exists = os.path.exists(abs_file_path)
-    print(f'Exists - {exists}')
-    if not exists:
-        os.makedirs(abs_file_path)
-
-    # if 
-    # if base_file_path not in working_dir_contents:
-    #     return(False, f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory') 
     
-    # if not (os.path.exists(os.path.join(working_dir_path, file_path))):
-    #     os.makedirs(os.join(working_dir_path, file_path))
+    if not exists:
+        os.makedirs(parent_dir, exist_ok=True)
+        with open(abs_file_path, 'w') as f:
+            f.write(content)
+        return(True, f"Directory path created and Content has been written to {abs_file_path}")
+    
+    
+    with open(abs_file_path, 'w') as f:
+        f.write(content)
+    return(True, f"Directory path and file exist content has been overwritten at  {abs_file_path}")    
+ 
+    
+
     
         

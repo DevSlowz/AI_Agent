9ec6bf808445521f2bd4015b22929bae40030b5a
DevSlowz <msvieira021@gmail.com>
2025-07-28 18:51:25 -0400

    Add system prompt string to feed to gemini api

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from google.genai import types
 import sys
 
 
-
+SYSTEM_PROMPT = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
 def main():
     # Load enviorment variables
     load_dotenv()
@@ -34,25 +34,28 @@ def main():
     if verbose:
         print(f"User prompt: {user_prompt}\n")
 
-        # Store conversation history and assign roles
-        messages = [
-            types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-        ]
-        generate_content(client,messages,verbose)
+    # Store conversation history and assign roles
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    generate_content(client,messages,verbose)
 
 
 
 def generate_content(client, messages, verbose):
     # Generate reponse based on messages argument
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents=messages,
+        model='gemini-2.0-flash-001', 
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=SYSTEM_PROMPT),
     )
 
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        print("Response:")
-        print(response.text)
+        
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":

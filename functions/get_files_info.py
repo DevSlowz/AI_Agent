e7175a7943185b868d6b36259247db80c092ef48
DevSlowz <msvieira021@gmail.com>
2025-07-29 07:59:20 -0400

    Add schema_get_files_info to define the blueprint for when the AI Agent wants to call the get_files_info function

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+import types
 
 
 def get_files_info(working_directory, directory=None):
@@ -85,3 +86,18 @@ def valid_directory(working_directory, directory):
             return (True, "", abs_path)
         
     return (False, err, abs_path)
+
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)

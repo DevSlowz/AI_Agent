d43b9ea06420b432efa0da9a3fd5db85b8346ade
DevSlowz <msvieira021@gmail.com>
2025-08-18 07:54:10 -0400

    Add a dictionary of functions for the AI Agent to call

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ from functions.get_file_content import schema_get_file_content
 from functions.run_python import schema_run_python_file
 from functions.write_file import schema_write_file
 
+from functions.write_file import write_file
+from functions.run_python import run_python_file
+from functions.get_file_content import get_file_content
+from functions.get_files_info import get_files_info
+
 system_prompt = """
 You are a helpful AI coding agent.
 
@@ -30,6 +35,34 @@ avaliable_functions = types.Tool(
         schema_write_file,
     ]
 )
+
+avaliable_function_calls = {
+    "write_file": write_file,
+    "run_python_file": run_python_file,
+    "get_file_content": get_file_content,
+    "get_files_info": get_files_info,
+}
+
+def call_function(function_call_part, verbose=False):
+    name = ""
+    args = ""
+    if verbose == False:
+        for function_call_part in function_call_part.function_calls:
+            print(f" - Calling function: {function_call_part.name}")
+        return
+
+    for function_call_part in function_call_part.function_calls:
+        function_call_part.args['working_directory'] = './calculator'
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        name = function_call_part.name
+        args = function_call_part.args
+    
+    print(name)
+    print(args)
+    # result = eval(f'{name}({**args})')
+    # print(result)
+    result = avaliable_function_calls[name](working_directory='./calculator', file_path=args['file_path'], content=args['content'])
+    print(result)
 def main():
 
     # Load enviorment variables
@@ -78,13 +111,23 @@ def generate_content(client, messages, verbose):
         
     )
 
+    print("Response:")
+
     if verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        
-    print("Response:")
-    print(response.text)
-    print(f"Calling function: {response.function_calls[0].name}({response.function_calls[0].args})")
+        call_function(response, verbose)
+
+    if not response.function_calls:
+        return response.text
+
+    call_function(response)
+    
+
+    # print(response.text)
+    
+
+
 
 
 if __name__ == "__main__":
